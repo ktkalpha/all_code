dd55c3bdbc29e4cad6074697acb80ca8ddd32c19
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2020-05-22 10:50:25 +0900

    Update 날씨.py

--- 날씨.py
+++ 날씨.py
@@ -7,4 +7,4 @@ with urlopen('https://search.naver.com/search.naver?ie=UTF-8&sm=whl_hty&query=%E
     soup = BeautifulSoup(response, 'html.parser')
     for anchor in soup.find_all('div', {'class' : 'main_info' }):
         n.append(anchor.get_text())
-pyautogui.alert(n[0][0:len('   16도씨℃    흐림, 어제보다 0˚ 높아요')])
+pyautogui.alert(n[0][0:len('   16도씨℃    흐림, 어제보다 0˚ 높아요')].replace('    ', ' '))
