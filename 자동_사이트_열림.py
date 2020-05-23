fc101f8db5f19c78b8cb567b8589c1cb14c1a1fc
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2020-05-23 11:54:28 +0900

    Update 자동_사이트_열림.py

--- 자동_사이트_열림.py
+++ 자동_사이트_열림.py
@@ -3,9 +3,10 @@ import webbrowser
 import sys
 시=0 #<-켜지는 시
 분=0 #<-켜지는 분
+사이트_주소='' #<-사이트 주소
 a=time.localtime()
 while True:
     if a.tm_hour==시 and a.tm_min==분:
-        webbrowser.open('#사이트_주소')
+        webbrowser.open(사이트_주소)
         break
 sys.exit()
