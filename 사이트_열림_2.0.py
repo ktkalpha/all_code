c2b8f7b63732abfa653882d7c8e87b4a45b899f6
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2020-05-23 12:33:11 +0900

    Add files via upload

---
+++ 사이트_열림_2.0.py
@@ -0,0 +1,13 @@
+import time
+import webbrowser
+import sys
+chrome_path = 'C:/Program Files (x86)/Naver/Naver Whale/Application/whale.exe %s'
+시=12 #<-켜지는 시
+분=26 #<-켜지는 분
+사이트_주소='naver.com' #<-사이트 주소
+while True:
+    a = time.gmtime()
+    if a.tm_hour+9==시 and a.tm_min==분:
+        webbrowser.get(chrome_path).open(사이트_주소)
+        break
+sys.exit()
