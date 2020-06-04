d42dca4e86adb8192a589b3ee8c7ffa30036ea39
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2020-06-04 19:18:20 +0900

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,14 @@
+#2
+def para1_func(n1,n2):
+ return n1+n2
+def para2_func(n1,n2,n3):
+ return n1+n2+n3
+print(para1_func(10,20))
+print(para2_func(10,20,30))
+#3
+def para1_func():
+ return 10+20
+def para2_func():
+ return 10+20+30
+print(para1_func())
+print(para2_func())
