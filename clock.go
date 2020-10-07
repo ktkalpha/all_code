1d0f768936ee051c99cccd5da9b41896c3d63b10
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2020-10-07 21:59:55 +0900

    Update clock.go

--- clock.go
+++ clock.go
@@ -2,32 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 )
 
-var now_hour, now_min, now_sec string
+func cls() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
 
 func main() {
 	for true {
 		if time.Now().Hour() > 12 {
-			now_hour = fmt.Sprintf("%d", time.Now().Hour()-12)
+			fmt.Println(fmt.Sprintf("%d:%d", time.Now().Hour()-12, time.Now().Minute()))
 		} else {
-			now_hour = fmt.Sprintf("%d", time.Now().Hour())
+			fmt.Println(fmt.Sprintf("%d:%d", time.Now().Hour(), time.Now().Minute()))
 		}
-		now_min = fmt.Sprintf("%d", time.Now().Minute())
-		now_sec = fmt.Sprintf("%d", time.Now().Second())
-		if len(now_hour) == 1 {
-			now_hour = fmt.Sprintf("%s", "0"+now_hour)
-		}
-		if len(now_min) == 1 {
-			now_min = fmt.Sprintf("%s", "0"+now_min)
-		}
-		if len(now_sec) == 1 {
-			now_sec = fmt.Sprintf("%s", "0"+now_sec)
-		}
-		now_time := fmt.Sprintf("%s:%s:%s\nktkalpha", now_hour, now_min, now_sec)
-		fmt.Println(now_time)
-		time.Sleep(100)
-		fmt.Println("\n\n\n\n\n\n\n\n")
+		time.Sleep(100 * time.Millisecond)
+		cls()
 	}
 }
